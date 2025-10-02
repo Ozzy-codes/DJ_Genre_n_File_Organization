b6159f48bea70a1253270f4f8a5e4c182dc892cb
Osvaldo Mesa <d3v.ozzy@gmail.com>
2025-10-02 08:24:36 -0400

    update transfer_to_drive, handle_file_txf, local and global variables

--- src/transfer_to_drive.sh
+++ src/transfer_to_drive.sh
@@ -1,9 +1,10 @@
 #!/opt/homebrew/bin/bash
 
 # first argument is the directory of which has the folders you want to extract files from e.g. the dirname which holds the name of genre directories with music files within them
-local local_genre_locations="${1:-"~/Music/mp3_genres/"}"
+local_genre_locations="${1:-"~/Music/mp3_genres/"}"
 # second argument is the directory of which has the folder you want to move files to e.g. the file_collection/mp3-files on an external drive that will house all music file memory nodes before creating hard links
-local target_drive_path="${2:-"/Volumes/T7"}"
+target_drive_path="${2:-"/Volumes/T7"}"
+date_string="$(date "+%b%d_%y_%a")"
 
 fail() {
     echo "Error: $1" >&2
@@ -12,13 +13,19 @@ fail() {
 handle_file_transfer() {
   local genre_dir_path="$1"
   local target_drive_path=$2
-  genre_name="$(basename $genre_dir_path)"
-  file_genre_name="${genre_name}.txt"
-  date_string="$(date "+%b%d_%y_%a")"
+  local genre_name="$(basename $genre_dir_path)"
+  local file_genre_name="${genre_name}.txt"
+  genre_file_name_path="src/genre_file_list_names/${file_genre_name}"
+
+  if test -z "$(ls "$genre_dir_path"/*)";then
+    echo "directory: $genre_dir_path, is empty"
+    return 
+  fi
 
   if test ! -d "$target_drive_path"/file_collection/mp3-files/${date_string}/${genre_name};then 
     mkdir -p "$target_drive_path"/file_collection/mp3-files/${date_string}/${genre_name}
   fi
+  # NOTE: target library is partitioned into date and genre based on incoming dir 
   if test ! -d src/genre_file_list_names/${date_string}; then 
     mkdir -p src/genre_file_list_names/${date_string}
   fi
@@ -30,6 +37,11 @@ handle_file_transfer() {
     echo "moving files to storage"
     mv "$genre_dir_path"/* "$target_drive_path"/file_collection/mp3-files/${date_string}/${genre_name}
   fi
+   # TODO: plan --- generating hard links will be dependent on file_genre_name; it will scan the content for the target files that need to linked to the appropriate genre. It will compare its contents to the current files in the mp3-files/date_string/genre_name.
+   # For every hit, it will link the file to the genre directory. Once the link operation is complete, it will perform clean up and delete the file
+  find "$genre_dir_path"/* -exec basename {} ";" >> $genre_file_name_path
+  mv "$genre_dir_path"/* "$target_drive_path"/file_collection/mp3-files/${date_string}/${genre_name}
+  echo "moved files to storage"
 }
 generate_hard_links() {
   local drive_base_path=$1
